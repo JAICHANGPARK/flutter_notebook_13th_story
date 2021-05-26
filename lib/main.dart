c6dc980a73c1a6c8b120efc5e32b3d57cd044610
Dreamwalker <aristojeff@gmail.com>
2021-05-26 20:46:35 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 
 
-import 'package:flutter/cupertino.dart';
 
-void main() => runApp(SnackProductApp())
+
+import 'package:flutter/material.dart';
+
+import 'ep801_snack_product_app/snack_product_app.dart';
+
+void main() => runApp(const SnackProductApp());
