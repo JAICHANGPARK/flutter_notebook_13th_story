057799a3ba272faf65d280eb3622ef22c4814427
Dreamwalker <aristojeff@gmail.com>
2021-05-26 20:58:33 +0900

    Update snack_product_main_page.dart

--- lib/ep801_snack_product_app/snack_product_main_page.dart
+++ lib/ep801_snack_product_app/snack_product_main_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-
 class SnackProductMainPage extends StatefulWidget {
   const SnackProductMainPage({Key? key}) : super(key: key);
 
@@ -14,42 +12,61 @@ class _SnackProductMainPageState extends State<SnackProductMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: [
-                  Expanded(child:
+        backgroundColor: Colors.grey[100],
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.arrow_back_ios),
+                            padding: EdgeInsets.zero,
+                            iconSize: 16,
+                          ),
+                          Text(
+                            "BACK",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      )),
 
-
-                  Row(children: [
-
-                    IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios), padding: EdgeInsets.zero, iconSize: 16,),
-                    Text("BACK", style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),),
-                  ],)),
-
-
-                  Expanded(child: Center(child: Text("SEARCH"))),
-                  Expanded(child: Align(
-
-                      alignment: Alignment.centerRight,
-                      child: CircleAvatar(backgroundColor: Colors.orange,))),
-                  // CircleAvatar(),
-
-
-                ],
+                      Expanded(
+                          child: Center(
+                              child: Text(
+                        "SEARCH",
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      ))),
+                      Expanded(
+                          child: Align(
+                              alignment: Alignment.centerRight,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.orange,
+                              ))),
+                      // CircleAvatar(),
+                    ],
+                  ),
+                ),
+                flex: 2,
               ),
-            ), flex: 2,),
-            Expanded(child: Placeholder(), flex: 2,),
-            Expanded(child: Placeholder(), flex: 15,),
-          ],
-        ),
-      )
-    );
+              Expanded(
+                child: Placeholder(),
+                flex: 2,
+              ),
+              Expanded(
+                child: Placeholder(),
+                flex: 15,
+              ),
+            ],
+          ),
+        ));
   }
 }
