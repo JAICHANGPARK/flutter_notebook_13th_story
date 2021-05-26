97c7a47e0c5c61fb6dce48a977cdcf20ffa21307
Dreamwalker <aristojeff@gmail.com>
2021-05-26 20:46:56 +0900

    Update snack_product_main_page.dart

--- lib/ep801_snack_product_app/snack_product_main_page.dart
+++ lib/ep801_snack_product_app/snack_product_main_page.dart
@@ -2,3 +2,16 @@ import 'package:flutter/material.dart';
 
 
 
+class SnackProductMainPage extends StatefulWidget {
+  const SnackProductMainPage({Key? key}) : super(key: key);
+
+  @override
+  _SnackProductMainPageState createState() => _SnackProductMainPageState();
+}
+
+class _SnackProductMainPageState extends State<SnackProductMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
