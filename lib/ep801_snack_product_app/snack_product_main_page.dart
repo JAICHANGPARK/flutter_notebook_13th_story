42b0084550100265750451089035569c5d51e956
Dreamwalker <aristojeff@gmail.com>
2021-05-26 20:45:53 +0900

    Update snack_product_main_page.dart

--- lib/ep801_snack_product_app/snack_product_main_page.dart
+++ lib/ep801_snack_product_app/snack_product_main_page.dart
@@ -1,13 +1,4 @@
 import 'package:flutter/material.dart';
 
 
-class SnackProductApp extends StatelessWidget {
-  const SnackProductApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      title: "Flutter Live Coding",
-    );
-  }
-}
