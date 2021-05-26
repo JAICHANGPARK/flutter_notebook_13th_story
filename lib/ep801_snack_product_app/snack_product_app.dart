c172e0446348545db67e53c018edd4146683ae36
Dreamwalker <aristojeff@gmail.com>
2021-05-26 20:45:49 +0900

    Create snack_product_app.dart

---
+++ lib/ep801_snack_product_app/snack_product_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class SnackProductApp extends StatelessWidget {
+  const SnackProductApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      title: "Flutter Live Coding",
+    );
+  }
+}
