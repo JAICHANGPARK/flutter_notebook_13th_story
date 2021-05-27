73f9ce9d22b8da36e2416e0c8ea198f85ca48f00
Dreamwalker <aristojeff@gmail.com>
2021-05-27 20:43:06 +0900

    Update snack_product_app.dart

--- lib/ep801_snack_product_app/snack_product_app.dart
+++ lib/ep801_snack_product_app/snack_product_app.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'snack_product_main_page.dart';
 
-
-
 class SnackProductApp extends StatelessWidget {
   const SnackProductApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return const MaterialApp(
       title: "Flutter Live Coding",
       home: SnackProductMainPage(),
     );
