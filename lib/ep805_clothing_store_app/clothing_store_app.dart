4081378f0473abbe937765bc6436028cc9771485
Dreamwalker <aristojeff@gmail.com>
2021-05-30 20:09:59 +0900

    Update clothing_store_app.dart

---
+++ lib/ep805_clothing_store_app/clothing_store_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ClothingStoreApp extends StatelessWidget {
+  const ClothingStoreApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ClothingStoreMainPage(),
+    );
+  }
+}
