b22c386c26233e57c59f527050c3936781025905
Dreamwalker <aristojeff@gmail.com>
2021-05-30 20:11:08 +0900

    Update clothing_store_main_page.dart

--- lib/ep805_clothing_store_app/ui/clothing_store_main_page.dart
+++ lib/ep805_clothing_store_app/ui/clothing_store_main_page.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
 
+class ClothingStoreMainPage extends StatefulWidget {
+  const ClothingStoreMainPage({Key? key}) : super(key: key);
+
+  @override
+  _ClothingStoreMainPageState createState() => _ClothingStoreMainPageState();
+}
+
+class _ClothingStoreMainPageState extends State<ClothingStoreMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
