0ddda220e3bbcd249e804497d5df2d2ddcf219f2
Dreamwalker <aristojeff@gmail.com>
2021-05-31 21:17:35 +0900

    Update clothing_store_main_page.dart

--- lib/ep805_clothing_store_app/ui/clothing_store_main_page.dart
+++ lib/ep805_clothing_store_app/ui/clothing_store_main_page.dart
@@ -59,7 +59,7 @@ class _ClothingStoreMainPageState extends State<ClothingStoreMainPage> {
             ),
             Expanded(
               child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 8),
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                 decoration: BoxDecoration(
                   color: Colors.white,
 
