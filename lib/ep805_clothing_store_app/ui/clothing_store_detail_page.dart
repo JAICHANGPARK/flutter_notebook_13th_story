6d7ca28b5c4afff2f26ef5ee596cfbb4d87da6c1
Dreamwalker <aristojeff@gmail.com>
2021-06-02 21:14:35 +0900

    Update clothing_store_detail_page.dart

--- lib/ep805_clothing_store_app/ui/clothing_store_detail_page.dart
+++ lib/ep805_clothing_store_app/ui/clothing_store_detail_page.dart
@@ -21,7 +21,6 @@ class _ClothingStoreDetailPageState extends State<ClothingStoreDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-
         child: Stack(
           children: [
             Positioned(
@@ -30,76 +29,79 @@ class _ClothingStoreDetailPageState extends State<ClothingStoreDetailPage> {
               right: 0,
               top: 0,
               child: Container(
-
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                image: DecorationImage(
-                  image: NetworkImage(widget.imgString),
-                  fit: BoxFit.cover
-                )
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: DecorationImage(image: NetworkImage(widget.imgString), fit: BoxFit.cover)),
               ),
-            ),),
+            ),
             Positioned(
                 top: 16,
                 left: 16,
                 right: 16,
-                child: Row(children: [
-              InkWell(
-                onTap: (){
-                  Navigator.pop(context);
-                },
-                  child: Icon(Icons.arrow_back)),
-              Spacer(),
-              Icon(Icons.favorite, color: Colors.red,),
-                  Container(
-                    height: 38,
-                    width: 38,
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          child: Icon(Icons.shopping_bag_outlined),
-                          left: 8,
-                          top: 8,
-                          right: 8,
-                          bottom: 8,
-                        ),
-                        Positioned(
-                          child: CircleAvatar(
-                            radius: 8,
-                            child: Text("3"),
-                            backgroundColor: Colors.red,
-                            foregroundColor: Colors.white,
-                          ),
-                          right: 0,
-                          top: 0,
-                        ),
-                      ],
+                child: Row(
+                  children: [
+                    InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(Icons.arrow_back)),
+                    Spacer(),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.red,
                     ),
-                  )
-            ],)),
+                    Container(
+                      height: 38,
+                      width: 38,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            child: Icon(Icons.shopping_bag_outlined),
+                            left: 8,
+                            top: 8,
+                            right: 8,
+                            bottom: 8,
+                          ),
+                          Positioned(
+                            child: CircleAvatar(
+                              radius: 8,
+                              child: Text("3"),
+                              backgroundColor: Colors.red,
+                              foregroundColor: Colors.white,
+                            ),
+                            right: 0,
+                            top: 0,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )),
             Positioned(
                 left: 16,
                 right: 16,
                 top: 24,
                 bottom: 200,
                 child: Container(
-              child: Center(
-                child: Row(
-                  children: [
-                      IconButton(onPressed: (){}, icon: Icon(Icons.keyboard_arrow_left)),
-                    IconButton(onPressed: (){}, icon: Icon(Icons.keyboard_arrow_right)),
-                  ],
-                ),
-              ),
-            )),
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconButton(onPressed: () {}, icon: Icon(Icons.keyboard_arrow_left)),
+                        IconButton(onPressed: () {}, icon: Icon(Icons.keyboard_arrow_right)),
+                      ],
+                    ),
+                  ),
+                )),
             Positioned(
               child: Container(
                 height: 200,
-                decoration: BoxDecoration(color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(16),
-                  topLeft:  Radius.circular(16),
-                )),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(16),
+                      topLeft: Radius.circular(16),
+                    )),
               ),
               left: 0,
               right: 0,
