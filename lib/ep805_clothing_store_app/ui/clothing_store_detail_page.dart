53001403f8fedf16c398c3e475ad436a8962a7ce
Dreamwalker <aristojeff@gmail.com>
2021-06-02 21:01:48 +0900

    Create clothing_store_detail_page.dart

---
+++ lib/ep805_clothing_store_app/ui/clothing_store_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ClothingStoreDetailPage extends StatefulWidget {
+  const ClothingStoreDetailPage({Key? key}) : super(key: key);
+
+  @override
+  _ClothingStoreDetailPageState createState() => _ClothingStoreDetailPageState();
+}
+
+class _ClothingStoreDetailPageState extends State<ClothingStoreDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
