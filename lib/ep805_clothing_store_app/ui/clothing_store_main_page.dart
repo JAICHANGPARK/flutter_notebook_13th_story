d86e6ce526365b40558881cb3ed32d16c06b1c8c
Dreamwalker <aristojeff@gmail.com>
2021-06-02 21:05:18 +0900

    Update clothing_store_main_page.dart

--- lib/ep805_clothing_store_app/ui/clothing_store_main_page.dart
+++ lib/ep805_clothing_store_app/ui/clothing_store_main_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
+import 'clothing_store_detail_page.dart';
+
 class ClothingStoreMainPage extends StatefulWidget {
   const ClothingStoreMainPage({Key? key}) : super(key: key);
 
@@ -144,75 +146,84 @@ class _ClothingStoreMainPageState extends State<ClothingStoreMainPage> {
                           mainAxisSpacing: 8,
                           itemCount: 8,
                           itemBuilder: (context, index) {
-                            return Container(
-                                child: Column(
-                              children: [
-                                Expanded(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image: NetworkImage(""
-                                                "https://cdn.pixabay.com/photo/2017/08/01/11/48/woman-2564660_960_720.jpg"),
-                                            fit: BoxFit.cover),
-                                        color: Colors.grey,
-                                        borderRadius: BorderRadius.circular(8)),
-                                    child: Stack(
-                                      children: [
-                                        Positioned(
-                                          right: 8,
-                                          top: 8,
-                                          child: CircleAvatar(
-                                            radius: 12,
-                                            backgroundColor: Colors.white,
-                                            foregroundColor: Colors.red,
-                                            child: Icon(
-                                              Icons.favorite,
-                                              size: 16,
+                            return InkWell(
+                              onTap: (){
+                                Navigator.push(context, MaterialPageRoute(builder: (context){
+                                  return ClothingStoreDetailPage(
+                                    
+                                  );
+                                }));
+                              },
+                              child: Container(
+                                  child: Column(
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          image: DecorationImage(
+                                              image: NetworkImage(""
+                                                  "https://cdn.pixabay.com/photo/2017/08/01/11/48/woman-2564660_960_720.jpg"),
+                                              fit: BoxFit.cover),
+                                          color: Colors.grey,
+                                          borderRadius: BorderRadius.circular(8)),
+                                      child: Stack(
+                                        children: [
+                                          Positioned(
+                                            right: 8,
+                                            top: 8,
+                                            child: CircleAvatar(
+                                              radius: 12,
+                                              backgroundColor: Colors.white,
+                                              foregroundColor: Colors.red,
+                                              child: Icon(
+                                                Icons.favorite,
+                                                size: 16,
+                                              ),
                                             ),
-                                          ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    flex: 8,
+                                  ),
+                                  SizedBox(
+                                    height: 8,
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Dream Walker - Jacket",
+                                          style: TextStyle(fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          height: 8,
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              '\$199.99',
+                                              style: TextStyle(fontWeight: FontWeight.bold),
+                                            ),
+                                            SizedBox(
+                                              width: 8,
+                                            ),
+                                            Text(
+                                              "\$299.99",
+                                              style:
+                                                  TextStyle(color: Colors.grey,
+                                                      decoration: TextDecoration.lineThrough),
+                                            )
+                                          ],
                                         )
                                       ],
                                     ),
+                                    flex: 4,
                                   ),
-                                  flex: 8,
-                                ),
-                                SizedBox(
-                                  height: 8,
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "Dream Walker - Jacket",
-                                        style: TextStyle(fontWeight: FontWeight.bold),
-                                      ),
-                                      SizedBox(
-                                        height: 8,
-                                      ),
-                                      Row(
-                                        children: [
-                                          Text(
-                                            '\$199.99',
-                                            style: TextStyle(fontWeight: FontWeight.bold),
-                                          ),
-                                          SizedBox(
-                                            width: 8,
-                                          ),
-                                          Text(
-                                            "\$299.99",
-                                            style:
-                                                TextStyle(color: Colors.grey,
-                                                    decoration: TextDecoration.lineThrough),
-                                          )
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                  flex: 4,
-                                ),
-                              ],
-                            ));
+                                ],
+                              )),
+                            );
                           },
                           staggeredTileBuilder: (index) {
                             return StaggeredTile.count(1, index.isEven ? 2 : 1);
