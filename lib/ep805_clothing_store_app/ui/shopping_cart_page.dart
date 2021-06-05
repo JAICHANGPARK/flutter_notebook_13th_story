3e4b0285c6307f2b26b01528b155cbc1a86cc9ec
Dreamwalker <aristojeff@gmail.com>
2021-06-05 19:07:53 +0900

    Update shopping_cart_page.dart

---
+++ lib/ep805_clothing_store_app/ui/shopping_cart_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoppingChartPage extends StatefulWidget {
+  const ShoppingChartPage({Key? key}) : super(key: key);
+
+  @override
+  _ShoppingChartPageState createState() => _ShoppingChartPageState();
+}
+
+class _ShoppingChartPageState extends State<ShoppingChartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
