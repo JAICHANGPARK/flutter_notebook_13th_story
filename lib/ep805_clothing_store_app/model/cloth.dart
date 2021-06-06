d64794772d5de40d6b7deb64a6557c14d19bfd3e
Dreamwalker <aristojeff@gmail.com>
2021-06-06 19:09:52 +0900

    Update cloth.dart

--- lib/ep805_clothing_store_app/model/cloth.dart
+++ lib/ep805_clothing_store_app/model/cloth.dart
@@ -5,14 +5,14 @@ class Cloth {
   double? originPrice;
   double? discountPrice;
 
-  Cloth({this.img, this.name, this.size, this.originPrice,
-  this.discountPrice});
+  Cloth({this.img, this.name, this.size, this.originPrice, this.discountPrice});
 }
 
-
-List<Cloth> clothItems =[
-  Cloth(img: "https://cdn.pixabay.com/photo/2019/07/30/11/16/coffee-4372724__340.jpg",
-  name: "Dream Walker - Jacket",
-  size: "Extra Large (XL)",
-  price: "42.99")
+List<Cloth> clothItems = [
+  Cloth(
+      img: "https://cdn.pixabay.com/photo/2019/07/30/11/16/coffee-4372724__340.jpg",
+      name: "Dream Walker - Jacket",
+      size: "Extra Large (XL)",
+      originPrice: 59.99,
+      discountPrice: 42.99),
 ];
