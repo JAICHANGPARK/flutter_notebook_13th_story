6f25a738f70ffed3775603f746ef8dca90659215
Dreamwalker <aristojeff@gmail.com>
2021-06-07 20:39:49 +0900

    Update Task.dart

--- lib/ep813_task_manager_app/model/Task.dart
+++ lib/ep813_task_manager_app/model/Task.dart
@@ -1,9 +1,11 @@
 
 class Task{
 
-  String title;
-  String tag;
-  String startDate;
-  String endDate;
-  String datetime;
+  String? title;
+  String? tag;
+  String? startDate;
+  String? endDate;
+  String? datetime;
+
+  Task(this.title, this.tag, this.startDate, this.endDate, this.datetime);
 }
