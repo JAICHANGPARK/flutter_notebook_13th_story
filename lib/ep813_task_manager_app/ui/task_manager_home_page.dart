21cdba2d071e823aef7755624ca4530c89d44cf5
Dreamwalker <aristojeff@gmail.com>
2021-06-07 20:37:29 +0900

    Create task_manager_home_page.dart

---
+++ lib/ep813_task_manager_app/ui/task_manager_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskManagerHomePage extends StatefulWidget {
+  const TaskManagerHomePage({Key? key}) : super(key: key);
+
+  @override
+  _TaskManagerHomePageState createState() => _TaskManagerHomePageState();
+}
+
+class _TaskManagerHomePageState extends State<TaskManagerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
