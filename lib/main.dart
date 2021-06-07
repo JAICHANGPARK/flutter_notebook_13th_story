75ee0d497e0efaa7046b9cd2ad2459c63c4b387f
Dreamwalker <aristojeff@gmail.com>
2021-06-07 20:37:48 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'ep801_snack_product_app/snack_product_app.dart';
-import 'ep805_clothing_store_app/clothing_store_app.dart';
+import 'ep813_task_manager_app/task_manager_app.dart';
 
-void main() => runApp(ClothingStoreApp());
+void main() => runApp(TaskManagerApp());
