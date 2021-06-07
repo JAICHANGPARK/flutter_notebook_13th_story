2313571fff6cba5242e703423bd66e765f3134e1
Dreamwalker <aristojeff@gmail.com>
2021-06-07 20:36:48 +0900

    Update task_manager_app.dart

--- lib/ep813_task_manager_app/task_manager_app.dart
+++ lib/ep813_task_manager_app/task_manager_app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class TaskManagerApp extends StatelessWidget {
   const TaskManagerApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      home: ,
+    );
   }
 }
