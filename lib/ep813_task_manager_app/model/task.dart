5fa53e5168577537901bbc5d0e4f383734508ab1
Dreamwalker <aristojeff@gmail.com>
2021-06-08 20:35:00 +0900

    Update task.dart

--- lib/ep813_task_manager_app/model/task.dart
+++ lib/ep813_task_manager_app/model/task.dart
@@ -6,8 +6,10 @@ class Task{
   String? startDate;
   String? endDate;
   String? datetime;
+  int? commentCount;
 
-  Task({this.taskNumber, this.title, this.tag, this.startDate, this.endDate, this.datetime});
+  Task({this.taskNumber, this.title, this.tag, this.startDate, this.endDate, this.datetime,
+  });
 }
 
 List<Task> taskItem = [
@@ -15,6 +17,7 @@ List<Task> taskItem = [
     taskNumber: "216",
     title: "Incorrect action message",
     tag: "ux-issue",
-    
+    startDate: "June 8",
+    endDate: "June 10"
   ),
 ];
