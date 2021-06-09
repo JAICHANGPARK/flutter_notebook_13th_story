15667791b786f7a7a99a63c6d9a2c8608bf9874f
Dreamwalker <aristojeff@gmail.com>
2021-06-09 20:56:40 +0900

    Update task_edit_page.dart

---
+++ lib/ep813_task_manager_app/ui/task_edit_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_13th_story/ep813_task_manager_app/model/task.dart';
+
+
+class TaskEditPage extends StatefulWidget {
+  Task? task;
+  const TaskEditPage({Key? key, this.task}) : super(key: key);
+
+  @override
+  _TaskEditPageState createState() => _TaskEditPageState();
+}
+
+class _TaskEditPageState extends State<TaskEditPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
