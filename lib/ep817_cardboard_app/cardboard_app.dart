351ea5d1114c1b3265ff4d2c11ff8813dfc560a1
Dreamwalker <aristojeff@gmail.com>
2021-06-11 20:34:06 +0900

    Update cardboard_app.dart

--- lib/ep817_cardboard_app/cardboard_app.dart
+++ lib/ep817_cardboard_app/cardboard_app.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'ui/cardboard_main_page.dart';
+
 class CardboardApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "CardboardApp",
+      home: CardboardMainPage(),
     );
   }
 }
