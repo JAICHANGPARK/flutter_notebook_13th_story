a71bfdebc0264af19e8e230de6d171a8d9c3d46b
Dreamwalker <aristojeff@gmail.com>
2021-06-11 20:33:35 +0900

    Create cardboard_main_page.dart

---
+++ lib/ep817_cardboard_app/ui/cardboard_main_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class CardboardMainPage extends StatefulWidget {
+  CardboardMainPage({Key key}) : super(key: key);
+
+  @override
+  _CardboardMainPageState createState() => _CardboardMainPageState();
+}
+
+class _CardboardMainPageState extends State<CardboardMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
