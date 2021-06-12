a8e414c31f0d53df3041b9a193abba8c2443ffde
Dreamwalker <aristojeff@gmail.com>
2021-06-12 20:18:25 +0900

    Update cardboard_main_page.dart

--- lib/ep817_cardboard_app/ui/cardboard_main_page.dart
+++ lib/ep817_cardboard_app/ui/cardboard_main_page.dart
@@ -20,32 +20,37 @@ class _CardboardMainPageState extends State<CardboardMainPage> {
       ),
       body: Column(
         children: [
-          Expanded(child: Placeholder(), flex: 3,),
-          Divider(color: Colors.grey,
-          thickness: 2,
+          Expanded(
+            child: Placeholder(),
+            flex: 3,
           ),
-          Expanded(child: Placeholder(), flex: 9,),
+          Divider(
+            color: Colors.grey,
+            thickness: 2,
+          ),
+          Expanded(
+            child: Placeholder(),
+            flex: 9,
+          ),
         ],
       ),
       floatingActionButton: InkWell(
-        onTap: (){
-           // ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(content: Text("asd"), actions: [
-           //   TextButton(
-           //     child: Text("Close"),
-           //     onPressed: (){
-           //       Navigator.of(context).pop();
-           //     },
-           //   )
-           // ]));
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('on'
-              'Button pressed record play')));
+        onTap: () {
+          // ScaffoldMessenger.of(context).showMaterialBanner(MaterialBanner(content: Text("asd"), actions: [
+          //   TextButton(
+          //     child: Text("Close"),
+          //     onPressed: (){
+          //       Navigator.of(context).pop();
+          //     },
+          //   )
+          // ]));
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text('on'
+                  'Button pressed record play')));
         },
         child: Container(
           padding: EdgeInsets.symmetric(horizontal: 32, vertical: 24),
-          decoration: BoxDecoration(
-            color: CardboardAppStyle.mainColor,
-            borderRadius: BorderRadius.circular(8)
-          ),
+          decoration: BoxDecoration(color: CardboardAppStyle.mainColor, borderRadius: BorderRadius.circular(8)),
           child: Text(
             "Record Play",
             style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
