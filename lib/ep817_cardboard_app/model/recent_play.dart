8b78cdf33b818f598bc1c001ff4f8f657920b8bb
Dreamwalker <aristojeff@gmail.com>
2021-06-13 19:10:39 +0900

    Update recent_play.dart

--- lib/ep817_cardboard_app/model/recent_play.dart
+++ lib/ep817_cardboard_app/model/recent_play.dart
@@ -1,10 +1,11 @@
-class RecentPlay{
+class RecentPlay {
   String? img;
   num? rate;
   String? title;
   String? subtitle;
 
   RecentPlay({this.img, this.rate, this.title, this.subtitle});
+
   @override
   String toString() {
     // TODO: implement toString
