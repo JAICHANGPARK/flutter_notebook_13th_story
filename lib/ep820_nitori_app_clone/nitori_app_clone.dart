9f503e6e9857e99291ae95fc67937ea4910f0bbf
Dreamwalker <aristojeff@gmail.com>
2021-06-14 20:24:16 +0900

    Update nitori_app_clone.dart

--- lib/ep820_nitori_app_clone/nitori_app_clone.dart
+++ lib/ep820_nitori_app_clone/nitori_app_clone.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_13th_story/ep820_nitori_app_clone/ui/page/nitori_home_page.dart';
 import 'package:get/get.dart';
 
 class NitoriAppClone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-
+      home: NitoriHomePage(),
     );
   }
 }
