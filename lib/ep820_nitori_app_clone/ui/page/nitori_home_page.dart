b15b24d883a19869e80adaea68de38ad194d84ce
Dreamwalker <aristojeff@gmail.com>
2021-06-14 20:21:44 +0900

    Create nitori_home_page.dart

---
+++ lib/ep820_nitori_app_clone/ui/page/nitori_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NitoriHomePage extends StatefulWidget {
+  const NitoriHomePage({Key? key}) : super(key: key);
+
+  @override
+  _NitoriHomePageState createState() => _NitoriHomePageState();
+}
+
+class _NitoriHomePageState extends State<NitoriHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
