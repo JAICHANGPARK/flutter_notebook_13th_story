1cf8a7e640a9e254801636d03ba4c13c3355c4c1
Dreamwalker <aristojeff@gmail.com>
2021-06-15 20:47:11 +0900

    Update nitori_ouchi_page.dart

---
+++ lib/ep820_nitori_app_clone/ui/page/nitori_ouchi_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NitoriOuchiPage extends StatefulWidget {
+  const NitoriOuchiPage({Key? key}) : super(key: key);
+
+  @override
+  _NitoriOuchiPageState createState() => _NitoriOuchiPageState();
+}
+
+class _NitoriOuchiPageState extends State<NitoriOuchiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
