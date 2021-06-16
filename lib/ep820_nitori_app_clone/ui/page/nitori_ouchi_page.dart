4e9aaa57c3b86abac693677843ca1fcfd5faf4bb
Dreamwalker <aristojeff@gmail.com>
2021-06-16 20:24:38 +0900

    Update nitori_ouchi_page.dart

--- lib/ep820_nitori_app_clone/ui/page/nitori_ouchi_page.dart
+++ lib/ep820_nitori_app_clone/ui/page/nitori_ouchi_page.dart
@@ -28,131 +28,133 @@ class _NitoriOuchiPageState extends State<NitoriOuchiPage> with SingleTickerProv
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      body: SafeArea(
-        child: NestedScrollView(
-          controller: _scrollController,
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                pinned: true,
-                floating: true,
-                backgroundColor: Colors.white,
-                forceElevated: innerBoxIsScrolled,
-                flexibleSpace: FlexibleSpaceBar(
-                  collapseMode: CollapseMode.pin,
-                  background: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        IconButton(onPressed: (){}, icon:  Icon(Icons.menu, ), color:  NitoriAppStyle.mainColor,),
-                        Expanded(
-                          child: Container(
-                            height: 32,
-                            width: double.infinity,
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200],
-                              borderRadius: BorderRadius.circular(8)
-                            ),
+      body: NestedScrollView(
+        controller: _scrollController,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              pinned: true,
+              floating: true,
+              backgroundColor: Colors.white,
+              forceElevated: innerBoxIsScrolled,
+              flexibleSpace: FlexibleSpaceBar(
+                collapseMode: CollapseMode.pin,
+                background: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      IconButton(onPressed: (){}, icon:  Icon(Icons.menu, ), color:  NitoriAppStyle.mainColor,),
+                      Expanded(
+                        child: Container(
+                          height: 38,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[100],
+                            borderRadius: BorderRadius.circular(3)
+                          ),
+                          child: TextField(
+                            
                           ),
                         ),
+                      ),
+                      IconButton(onPressed: (){}, icon:  Icon(Icons.shopping_cart_outlined, ), color:  NitoriAppStyle.mainColor,),
+                    ],
+                  ),
+                ),
+              ),
+              elevation: 2,
+              bottom: TabBar(
+                labelColor: NitoriAppStyle.mainColor,
+                unselectedLabelColor: Colors.black87,
+                controller: _tabController,
+                indicatorColor: NitoriAppStyle.mainColor,
+                indicatorWeight: 2,
+                indicatorSize: TabBarIndicatorSize.label,
+                tabs: [
+                  Tab(
+                    text: "トピックス",
+                  ),
+                  Tab(
+                    text: "コーディネーター",
+                  ),
+                ],
+              ),
+            ),
+          ];
+        },
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, top: 8, bottom: 8),
+                  child: Container(
+                    height: 42,
+                    width: 180,
+                    decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(8)),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 48,
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                child: Icon(
+                                  Icons.list,
+                                  size: 32,
+                                  color: Colors.black45,
+                                ),
+                                right: 0,
+                                left: 0,
+                                bottom: 0,
+                                top: 0,
+                              ),
+                              Positioned(
+                                child: CircleAvatar(
+                                  radius: 4,
+                                  backgroundColor: Colors.red,
+                                ),
+                                right: 4,
+                                top: 8,
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        DropdownButton<String>(
+                            value: categories,
+                            isExpanded: false,
+                            onChanged: (s) {
+                              setState(() {
+                                categories = s;
+                              });
+                            },
+                            onTap: () {},
+                            items: [
+                              DropdownMenuItem(
+                                child: Text("カテゴリ"),
+                                value: "カテゴリ",
+                              ),
+                              DropdownMenuItem(
+                                child: Text("カテゴリ2"),
+                                value: "カテゴリ2",
+                              )
+                            ])
                       ],
                     ),
                   ),
                 ),
-                elevation: 2,
-                bottom: TabBar(
-                  labelColor: NitoriAppStyle.mainColor,
-                  unselectedLabelColor: Colors.black87,
-                  controller: _tabController,
-                  indicatorColor: NitoriAppStyle.mainColor,
-                  indicatorWeight: 2,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  tabs: [
-                    Tab(
-                      text: "トピックス",
-                    ),
-                    Tab(
-                      text: "コーディネーター",
-                    ),
-                  ],
-                ),
-              ),
-            ];
-          },
-          body: TabBarView(
-            controller: _tabController,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16, top: 8, bottom: 8),
-                    child: Container(
-                      height: 42,
-                      width: 180,
-                      decoration: BoxDecoration(color: Colors.grey[200], borderRadius: BorderRadius.circular(8)),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 48,
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                  child: Icon(
-                                    Icons.list,
-                                    size: 32,
-                                    color: Colors.black45,
-                                  ),
-                                  right: 0,
-                                  left: 0,
-                                  bottom: 0,
-                                  top: 0,
-                                ),
-                                Positioned(
-                                  child: CircleAvatar(
-                                    radius: 4,
-                                    backgroundColor: Colors.red,
-                                  ),
-                                  right: 4,
-                                  top: 8,
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            width: 16,
-                          ),
-                          DropdownButton<String>(
-                              value: categories,
-                              isExpanded: false,
-                              onChanged: (s) {
-                                setState(() {
-                                  categories = s;
-                                });
-                              },
-                              onTap: () {},
-                              items: [
-                                DropdownMenuItem(
-                                  child: Text("カテゴリ"),
-                                  value: "カテゴリ",
-                                ),
-                                DropdownMenuItem(
-                                  child: Text("カテゴリ2"),
-                                  value: "カテゴリ2",
-                                )
-                              ])
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(child: Placeholder()),
-                ],
-              ),
-              Container()
-            ],
-          ),
+                Expanded(child: Placeholder()),
+              ],
+            ),
+            Container()
+          ],
         ),
       ),
     );
