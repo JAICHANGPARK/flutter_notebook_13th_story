536bc000f2e23cb62597aac328e40f75e36de7a8
Dreamwalker <aristojeff@gmail.com>
2021-06-16 20:42:06 +0900

    Update nitori_topic_items.dart

--- lib/ep820_nitori_app_clone/mock/nitori_topic_items.dart
+++ lib/ep820_nitori_app_clone/mock/nitori_topic_items.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_notebook_13th_story/ep820_nitori_app_clone/model/nitori_topic.dart';
 
-
-
+DateTime nowDateTime = DateTime.now();
+List<NitoriTopic> nitoriTopicItems = [NitoriTopic(date: DateTime.now())];
