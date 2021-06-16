8440357cc01daac87a1554ae3d525165311df784
Dreamwalker <aristojeff@gmail.com>
2021-06-16 20:46:22 +0900

    Update nitori_topic_items.dart

--- lib/ep820_nitori_app_clone/mock/nitori_topic_items.dart
+++ lib/ep820_nitori_app_clone/mock/nitori_topic_items.dart
@@ -2,11 +2,12 @@ import 'package:flutter_notebook_13th_story/ep820_nitori_app_clone/enums/sns_typ
 import 'package:flutter_notebook_13th_story/ep820_nitori_app_clone/model/nitori_topic.dart';
 
 DateTime nowDateTime = DateTime.now();
+
 List<NitoriTopic> nitoriTopicItems = [
   NitoriTopic(
       date: "${nowDateTime.year / nowDateTime.month / nowDateTime.day}",
-      img: "",
-      title: "",
+      img: "https://cdn.pixabay.com/photo/2017/08/02/01/01/living-room-2569325_960_720.jpg",
+      title: "家具引取りサービス無料",
       isLike: false,
       likeCount: 0,
       type: SnsType.kiji)
