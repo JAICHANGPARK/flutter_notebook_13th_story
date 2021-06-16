40eeb5e132a43a22e02a13b2e40d0a874a81d9b2
Dreamwalker <aristojeff@gmail.com>
2021-06-16 20:32:56 +0900

    Update home_topics_widget.dart

--- lib/ep820_nitori_app_clone/ui/widgets/home_topics_widget.dart
+++ lib/ep820_nitori_app_clone/ui/widgets/home_topics_widget.dart
@@ -11,21 +11,33 @@ class HomeTopicsWidget extends StatefulWidget {
 class _HomeTopicsWidgetState extends State<HomeTopicsWidget> {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      itemCount: 10,
-      itemBuilder: (context, index){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.builder(
+        padding: EdgeInsets.zero,
+        itemCount: 10,
+        itemBuilder: (context, index){
 
-        return Container(
-          decoration: BoxDecoration(
-            color: Colors.grey
-          ),
-        );
-      }, gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: 2,
-      childAspectRatio: 1,
-      crossAxisSpacing: 4,
-      mainAxisSpacing: 4
-    ),
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.grey
+            ),
+            child: Column(
+              children: [
+                Expanded(child: Container(
+                  color: Colors.greenAccent,
+                ), flex: 4,),
+                Expanded(child: Placeholder(),flex: 2,),
+              ],
+            ),
+          );
+        }, gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.7,
+        crossAxisSpacing: 4,
+        mainAxisSpacing: 4
+      ),
+      ),
     );
   }
 }
