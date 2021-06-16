414745fef26abef6c79fec94ddf1bd1c5c32c7f5
Dreamwalker <aristojeff@gmail.com>
2021-06-16 20:37:25 +0900

    Update nitori_topic.dart

--- lib/ep820_nitori_app_clone/model/nitori_topic.dart
+++ lib/ep820_nitori_app_clone/model/nitori_topic.dart
@@ -1,9 +1,13 @@
 
 
+import '../enums/sns_type.dart';
+
 class nitoriTopic{
   String? date;
   String? title;
   String? img;
-  String? type; // sns, kiji,
-  int? count;
+  SnsType? type; // sns, kiji,
+  int? likeCount;
+  bool? isLike;
+
 }
