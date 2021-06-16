b9591be48427b2f46cb7f1ab450864fd38169336
Dreamwalker <aristojeff@gmail.com>
2021-06-16 20:43:16 +0900

    Update nitori_topic_items.dart

--- lib/ep820_nitori_app_clone/mock/nitori_topic_items.dart
+++ lib/ep820_nitori_app_clone/mock/nitori_topic_items.dart
@@ -1,4 +1,13 @@
+import 'package:flutter_notebook_13th_story/ep820_nitori_app_clone/enums/sns_type.dart';
 import 'package:flutter_notebook_13th_story/ep820_nitori_app_clone/model/nitori_topic.dart';
 
 DateTime nowDateTime = DateTime.now();
-List<NitoriTopic> nitoriTopicItems = [NitoriTopic(date: DateTime.now())];
+List<NitoriTopic> nitoriTopicItems = [
+  NitoriTopic(
+      date: "${nowDateTime.year / nowDateTime.month / nowDateTime.day}",
+      img: "",
+      title: "",
+      isLike: false,
+      likeCount: 0,
+      type: SnsType.kiji)
+];
