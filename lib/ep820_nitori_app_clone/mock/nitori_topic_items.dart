b95746bcf31e4d37e2bcc456f4e99675f6b9bb68
Dreamwalker <aristojeff@gmail.com>
2021-06-17 20:51:35 +0900

    Update nitori_topic_items.dart

--- lib/ep820_nitori_app_clone/mock/nitori_topic_items.dart
+++ lib/ep820_nitori_app_clone/mock/nitori_topic_items.dart
@@ -5,14 +5,14 @@ DateTime nowDateTime = DateTime.now();
 
 List<NitoriTopic> nitoriTopicItems = [
   NitoriTopic(
-      date: "${nowDateTime.year / nowDateTime.month / nowDateTime.day}",
+      date: "${nowDateTime.year} / ${nowDateTime.month} / ${nowDateTime.day}",
       img: "https://cdn.pixabay.com/photo/2017/08/02/01/01/living-room-2569325_960_720.jpg",
       title: "家具引取りサービス無料",
       isLike: false,
       likeCount: 0,
       type: SnsType.kiji),
   NitoriTopic(
-      date: "${nowDateTime.year / nowDateTime.month / nowDateTime.day}",
+      date: "${nowDateTime.year} / ${nowDateTime.month} / ${nowDateTime.day}",
       img: "https://cdn.pixabay.com/photo/2016/01/19/18/03/tea-1150046_960_720.jpg",
       title: "Dream 8月 オフショット",
       isLike: true,
