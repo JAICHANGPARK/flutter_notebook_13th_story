9af3f8634a5705d9217fb6f50734f8cd0e530ab1
Dreamwalker <aristojeff@gmail.com>
2021-06-18 20:44:33 +0900

    Create nitori_member_page.dart

---
+++ lib/ep820_nitori_app_clone/ui/page/nitori_member_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NitoriMemberPage extends StatefulWidget {
+  NitoriMemberPage({Key key}) : super(key: key);
+
+  @override
+  _NitoriMemberPageState createState() => _NitoriMemberPageState();
+}
+
+class _NitoriMemberPageState extends State<NitoriMemberPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
