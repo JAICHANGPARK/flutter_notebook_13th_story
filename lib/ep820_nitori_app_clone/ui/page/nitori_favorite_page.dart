7f514b8ed2401012a00a441456514b91b3dd146b
Dreamwalker <aristojeff@gmail.com>
2021-06-20 19:30:38 +0900

    Update nitori_favorite_page.dart

---
+++ lib/ep820_nitori_app_clone/ui/page/nitori_favorite_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NitoriFavoritePage extends StatefulWidget {
+  const NitoriFavoritePage({Key? key}) : super(key: key);
+
+  @override
+  _NitoriFavoritePageState createState() => _NitoriFavoritePageState();
+}
+
+class _NitoriFavoritePageState extends State<NitoriFavoritePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
