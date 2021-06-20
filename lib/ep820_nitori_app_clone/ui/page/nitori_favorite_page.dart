6ab4c13f06137419afe9186a6227c4ecdc0f5146
Dreamwalker <aristojeff@gmail.com>
2021-06-20 19:33:22 +0900

    Update nitori_favorite_page.dart

--- lib/ep820_nitori_app_clone/ui/page/nitori_favorite_page.dart
+++ lib/ep820_nitori_app_clone/ui/page/nitori_favorite_page.dart
@@ -13,7 +13,12 @@ class _NitoriFavoritePageState extends State<NitoriFavoritePage> {
     return SafeArea(
       child: Column(
         children: [
-          Expanded(child: Placeholder(), flex: 3,),
+          Expanded(child: Column(
+            children: [
+              Expanded(child: Placeholder()),
+              Expanded(child: Placeholder()),
+            ],
+          ), flex: 3,),
           Expanded(child: Placeholder(),flex: 12,),
           Expanded(child: Placeholder(), flex: 4,),
         ],
