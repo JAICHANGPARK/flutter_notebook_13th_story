39fd44291b7d97c9e124b72d49967aad66834894
Dreamwalker <aristojeff@gmail.com>
2021-06-22 20:28:27 +0900

    Update nitori_favorite_items.dart

--- lib/ep820_nitori_app_clone/mock/nitori_favorite_items.dart
+++ lib/ep820_nitori_app_clone/mock/nitori_favorite_items.dart
@@ -3,7 +3,13 @@
 import '../model/nitori_favorite.dart';
 
 List<NitoriFavotite> nitoriFavoriteItems = [
-  NitoriFavotite(),
+  NitoriFavotite(
+    img: "https://cdn.pixabay.com/photo/2018/11/20/09/48/carpets-showroom-3826924_960_720.jpg",
+    title: "シェニールジャガード　ラグ",
+    price: 6102,
+    itemCode: "7223834",
+    isSelect: true,
+  ),
   NitoriFavotite(),
   NitoriFavotite(),
 
