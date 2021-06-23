17a96826508dac762bc6618b120e6f8cc246179d
Dreamwalker <aristojeff@gmail.com>
2021-06-23 20:48:01 +0900

    Update fashion_store_app.dart

--- lib/ep829_fashion_store_app/fashion_store_app.dart
+++ lib/ep829_fashion_store_app/fashion_store_app.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
 
+class FashionStoreApp extends StatelessWidget {
+  const FashionStoreApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
