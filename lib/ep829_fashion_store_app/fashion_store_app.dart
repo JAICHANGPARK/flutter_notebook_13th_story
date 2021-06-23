5a37d36d25769ad4bfdfda5ecc22bfb431aa4a25
Dreamwalker <aristojeff@gmail.com>
2021-06-23 20:49:48 +0900

    Update fashion_store_app.dart

--- lib/ep829_fashion_store_app/fashion_store_app.dart
+++ lib/ep829_fashion_store_app/fashion_store_app.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'ui/fashion_main_page.dart';
+
 class FashionStoreApp extends StatelessWidget {
   const FashionStoreApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Fashion Store App"
+      title: "Fashion Store App",
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => FashionMainPage(),
+      },
     );
   }
 }
