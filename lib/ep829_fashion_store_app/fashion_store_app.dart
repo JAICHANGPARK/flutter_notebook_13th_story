e4f31254bc2fa0751b776ac09be54f7caa20aa0b
Dreamwalker <aristojeff@gmail.com>
2021-06-23 20:48:36 +0900

    Update fashion_store_app.dart

--- lib/ep829_fashion_store_app/fashion_store_app.dart
+++ lib/ep829_fashion_store_app/fashion_store_app.dart
@@ -5,6 +5,8 @@ class FashionStoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Fashion Store App"
+    );
   }
 }
