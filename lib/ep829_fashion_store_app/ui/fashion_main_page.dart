a13b1e7a00db939120919363a124bc6b24076c8a
Dreamwalker <aristojeff@gmail.com>
2021-06-23 20:49:45 +0900

    Create fashion_main_page.dart

---
+++ lib/ep829_fashion_store_app/ui/fashion_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FashionMainPage extends StatefulWidget {
+  FashionMainPage({Key key}) : super(key: key);
+
+  @override
+  _FashionMainPageState createState() => _FashionMainPageState();
+}
+
+class _FashionMainPageState extends State<FashionMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
