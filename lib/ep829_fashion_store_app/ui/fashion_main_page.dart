18845d7d05d2a782f3509c80730be45086c64ca3
Dreamwalker <aristojeff@gmail.com>
2021-06-23 20:53:41 +0900

    Update fashion_main_page.dart

--- lib/ep829_fashion_store_app/ui/fashion_main_page.dart
+++ lib/ep829_fashion_store_app/ui/fashion_main_page.dart
@@ -14,7 +14,20 @@ class _FashionMainPageState extends State<FashionMainPage> {
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(child: Placeholder(), flex: 8,),
+            Expanded(child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(24)
+                ),
+                child: Stack(
+                  children: [
+
+                  ],
+                ),
+              ),
+            ), flex: 8,),
             Expanded(child: Placeholder(), flex: 3,),
             Expanded(child: Placeholder(), flex: 2,),
           ],
