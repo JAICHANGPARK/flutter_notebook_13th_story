5f1abb2bdf2d13af84f8fb384b4fb59200b3ace5
Dreamwalker <aristojeff@gmail.com>
2021-06-25 21:05:15 +0900

    Update shopping_bag_page.dart

--- lib/ep829_fashion_store_app/ui/shopping_bag_page.dart
+++ lib/ep829_fashion_store_app/ui/shopping_bag_page.dart
@@ -11,6 +11,7 @@ class _ShoppingBagPageState extends State<ShoppingBagPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Column(
           children: [
