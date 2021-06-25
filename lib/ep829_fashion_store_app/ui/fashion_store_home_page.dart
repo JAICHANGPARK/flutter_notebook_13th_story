955d5dbaef2bb813895c9a39be7a183d173d953d
Dreamwalker <aristojeff@gmail.com>
2021-06-25 20:48:11 +0900

    Update fashion_store_home_page.dart

--- lib/ep829_fashion_store_app/ui/fashion_store_home_page.dart
+++ lib/ep829_fashion_store_app/ui/fashion_store_home_page.dart
@@ -4,16 +4,18 @@ class FashionStoreHomePage extends StatefulWidget {
   FashionStoreHomePage({Key? key}) : super(key: key);
 
   @override
-  _FashionStoreMainPageState createState() => _FashionStoreMainPageState();
+  _FashionStoreHomePageState createState() => _FashionStoreHomePageState();
 }
 
-class _FashionStoreMainPageState extends State<FashionStoreHomePage> {
+class _FashionStoreHomePageState extends State<FashionStoreHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          OutlinedButton(onPressed: (){}, child: Text(""))
+          OutlinedButton(onPressed: (){}, child: Text("Main")),
+          OutlinedButton(onPressed: (){}, child: Text("Shopping Bag"))
         ],
       ),
     );
