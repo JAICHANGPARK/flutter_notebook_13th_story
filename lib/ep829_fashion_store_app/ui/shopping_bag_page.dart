ada461aa9af0d8bcdaf49cbe97a793871083ad2a
Dreamwalker <aristojeff@gmail.com>
2021-06-25 20:44:30 +0900

    Update shopping_bag_page.dart

--- lib/ep829_fashion_store_app/ui/shopping_bag_page.dart
+++ lib/ep829_fashion_store_app/ui/shopping_bag_page.dart
@@ -1,1 +1,17 @@
 
+import 'package:flutter/material.dart';
+
+
+class ShoppingBagPage extends StatefulWidget {
+  const ShoppingBagPage({Key? key}) : super(key: key);
+
+  @override
+  _ShoppingBagPageState createState() => _ShoppingBagPageState();
+}
+
+class _ShoppingBagPageState extends State<ShoppingBagPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
