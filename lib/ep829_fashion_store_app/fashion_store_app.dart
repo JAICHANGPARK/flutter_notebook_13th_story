6c6346467a00e1d5658374badf2f39ffb68dfdc5
Dreamwalker <aristojeff@gmail.com>
2021-06-25 20:48:54 +0900

    Update fashion_store_app.dart

--- lib/ep829_fashion_store_app/fashion_store_app.dart
+++ lib/ep829_fashion_store_app/fashion_store_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_13th_story/ep829_fashion_store_app/ui/shopping_bag_page.dart';
 
 import 'ui/fashion_main_page.dart';
+import 'ui/fashion_store_home_page.dart';
 
 class FashionStoreApp extends StatelessWidget {
   const FashionStoreApp({Key? key}) : super(key: key);
@@ -11,7 +13,9 @@ class FashionStoreApp extends StatelessWidget {
       title: "Fashion Store App",
       initialRoute: "/",
       routes: {
-        "/" : (context) => FashionMainPage(),
+        "/" : (context) => FashionStoreHomePage(),
+        "/main" :(context) => FashionMainPage(),
+        "/shopping_bag" :(context) => ShoppingBagPage(),
       },
     );
   }
