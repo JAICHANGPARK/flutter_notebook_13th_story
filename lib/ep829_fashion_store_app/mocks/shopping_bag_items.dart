0f4fccf40d99c0c7d82eb4244afbc714001271e7
Dreamwalker <aristojeff@gmail.com>
2021-06-26 20:03:50 +0900

    Update shopping_bag_items.dart

--- lib/ep829_fashion_store_app/mocks/shopping_bag_items.dart
+++ lib/ep829_fashion_store_app/mocks/shopping_bag_items.dart
@@ -7,7 +7,24 @@ List<ShoppingBag> shoppingBagItems = [
 
   ShoppingBag(
     count: 1,
-    price: 
+    price: 159.00,
+    img: "https://cdn.pixabay.com/photo/2015/03/26/09/40/suit-690048__340.jpg",
+    title: "Jacket",
+    size: "L"
+  ),
+  ShoppingBag(
+      count: 1,
+      price: 109.00,
+      img: "https://cdn.pixabay.com/photo/2017/08/01/11/48/woman-2564660_960_720.jpg",
+      title: "Jeans jacket",
+      size: "L"
+  ),
+  ShoppingBag(
+      count: 1,
+      price: 999.00,
+      img: "https://cdn.pixabay.com/photo/2017/10/19/11/00/pendant-2867205_960_720.jpg",
+      title: "Crown",
+      size: "L"
   ),
 
 ];
