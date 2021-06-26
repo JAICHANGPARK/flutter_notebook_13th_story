36955fd70feb651a1c1a55c67dea022bd9e0b4f9
Dreamwalker <aristojeff@gmail.com>
2021-06-26 19:57:02 +0900

    Update shopping_bag_items.dart

--- lib/ep829_fashion_store_app/mocks/shopping_bag_items.dart
+++ lib/ep829_fashion_store_app/mocks/shopping_bag_items.dart
@@ -1,5 +1,14 @@
 
 import 'package:flutter_notebook_13th_story/ep829_fashion_store_app/model/shopping_bag.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 
 List<ShoppingBag> shoppingBagItems = [];
+
+class shoppingBagList extends StateNotifier<List<shoppingBagList>>{
+  shoppingBagList(List<shoppingBagList>? state) : super(state ?? []);
+
+  
+
+
+}
