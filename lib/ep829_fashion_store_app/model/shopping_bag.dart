6185e02b791e041f87ed8c8b7ff969d8856785e2
Dreamwalker <aristojeff@gmail.com>
2021-06-26 19:53:14 +0900

    Update shopping_bag.dart

--- lib/ep829_fashion_store_app/model/shopping_bag.dart
+++ lib/ep829_fashion_store_app/model/shopping_bag.dart
@@ -6,4 +6,13 @@ class ShoppingBag {
   String? size;
 
   ShoppingBag({this.img, this.title, this.price, this.count, this.size});
+
+  @override
+  String toString() {
+    return "ShoppingBag("
+        "title: $title,"
+        "price: $price,"
+        "count: $count,"
+        "size: $size)";
+  }
 }
