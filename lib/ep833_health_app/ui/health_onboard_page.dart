ace405618932cb75e71ed2d159b534afa794b214
Dreamwalker <aristojeff@gmail.com>
2021-06-27 21:04:52 +0900

    Create health_onboard_page.dart

---
+++ lib/ep833_health_app/ui/health_onboard_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HealthOnboardPage extends StatefulWidget {
+  const HealthOnboardPage({Key? key}) : super(key: key);
+
+  @override
+  _HealthOnboardPageState createState() => _HealthOnboardPageState();
+}
+
+class _HealthOnboardPageState extends State<HealthOnboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
