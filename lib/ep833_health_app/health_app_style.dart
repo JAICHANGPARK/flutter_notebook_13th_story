c29d4082e5d808c8792ede611b95f22c3b70703d
Dreamwalker <aristojeff@gmail.com>
2021-06-27 21:07:25 +0900

    Create health_app_style.dart

---
+++ lib/ep833_health_app/health_app_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorSystem {
+  static const Color green = Color(0xff65B897);
+  static const Color yellow = Color(0xffF4DB71);
+  static const Color black = Color(0xff010001);
+  static const Color white = Color(0xffffffff);
+  static const Color grey = Color(0xfff2f2f2);
+}
