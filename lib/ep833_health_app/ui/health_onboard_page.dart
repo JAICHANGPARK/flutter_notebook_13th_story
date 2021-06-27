e7fe7496654949807846c8c5f0abe5b6c94719c4
Dreamwalker <aristojeff@gmail.com>
2021-06-27 21:08:17 +0900

    Update health_onboard_page.dart

--- lib/ep833_health_app/ui/health_onboard_page.dart
+++ lib/ep833_health_app/ui/health_onboard_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_13th_story/ep833_health_app/health_app_style.dart';
 
 
 class HealthOnboardPage extends StatefulWidget {
@@ -11,6 +12,13 @@ class HealthOnboardPage extends StatefulWidget {
 class _HealthOnboardPageState extends State<HealthOnboardPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ColorSystem.yellow,
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
