c586f1db20af7da8853bb166acaf6ec2d015cf1c
Dreamwalker <aristojeff@gmail.com>
2021-06-27 20:59:54 +0900

    Update health_app.dart

---
+++ lib/ep833_health_app/health_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class HealthApp extends StatelessWidget {
+  const HealthApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+
+    );
+  }
+}
