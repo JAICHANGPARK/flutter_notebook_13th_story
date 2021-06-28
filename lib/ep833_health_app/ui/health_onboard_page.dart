a238a8943304f7f568c0ac2fe7cce2ad6df40496
Dreamwalker <aristojeff@gmail.com>
2021-06-28 21:16:07 +0900

    Update health_onboard_page.dart

--- lib/ep833_health_app/ui/health_onboard_page.dart
+++ lib/ep833_health_app/ui/health_onboard_page.dart
@@ -65,7 +65,13 @@ class _HealthOnboardPageState extends State<HealthOnboardPage> {
                         ),
                       ],
                     )),
-                
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  top: 0,
+                  bottom: 0,
+                  child: Image.asset("");
+                ),
               ],
             ),
           ),
