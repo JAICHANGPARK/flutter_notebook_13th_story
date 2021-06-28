f2907bc6e5c0ba09e2e236341b28832faeda05b9
Dreamwalker <aristojeff@gmail.com>
2021-06-28 21:18:41 +0900

    Update health_app_string.dart

--- lib/ep833_health_app/health_app_string.dart
+++ lib/ep833_health_app/health_app_string.dart
@@ -1,5 +1,5 @@
 
 class HealthAppString{
   static const JOGGING_IMG = "assets/img/pixeltrue-jogging.svg";
-  static const CIRCLE_IMG = "assets/img/cicle.png";
+  static const CIRCLE_IMG = "assets/img/circle.png";
 }
