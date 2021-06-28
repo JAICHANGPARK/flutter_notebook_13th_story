347882ee21e60929561d7148a3303afc323ce985
Dreamwalker <aristojeff@gmail.com>
2021-06-28 21:27:08 +0900

    Update health_sign_up_page.dart

--- lib/ep833_health_app/ui/health_sign_up_page.dart
+++ lib/ep833_health_app/ui/health_sign_up_page.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+
+class HealthSignUpPage extends StatefulWidget {
+  const HealthSignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _HealthSignUpPageState createState() => _HealthSignUpPageState();
+}
+
+class _HealthSignUpPageState extends State<HealthSignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
