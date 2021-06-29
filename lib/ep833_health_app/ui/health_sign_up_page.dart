19e0f1c13983c74ad33af950b3f8a0732a9f70b0
Dreamwalker <aristojeff@gmail.com>
2021-06-29 20:52:44 +0900

    Update health_sign_up_page.dart

--- lib/ep833_health_app/ui/health_sign_up_page.dart
+++ lib/ep833_health_app/ui/health_sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HealthSignUpPage extends StatefulWidget {
   const HealthSignUpPage({Key? key}) : super(key: key);
@@ -26,12 +27,14 @@ class _HealthSignUpPageState extends State<HealthSignUpPage> {
                         crossAxisAlignment: CrossAxisAlignment.center,
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("Welcome to", style: TextStyle(
-                            fontSize: 38
-                          ),),
-                          Text("Ideate Fit", style: TextStyle(
-                              fontSize: 38
-                          ),),
+                          Text(
+                            "Welcome to",
+                            style: GoogleFonts.kanit(fontSize: 38),
+                          ),
+                          Text(
+                            "Ideate Fit",
+                            style: GoogleFonts.kanit(fontSize: 38),
+                          ),
                         ],
                       ))
                 ],
