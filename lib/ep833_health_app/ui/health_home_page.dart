dc3b4938d605dd8e67e3fe81450736f8875d7d91
Dreamwalker <aristojeff@gmail.com>
2021-06-30 23:13:05 +0900

    Update health_home_page.dart

--- lib/ep833_health_app/ui/health_home_page.dart
+++ lib/ep833_health_app/ui/health_home_page.dart
@@ -13,10 +13,21 @@ class _HealthHomePageState extends State<HealthHomePage> {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(child: Container(
-            height: 72,
-
-          ), left: 0, right: 0, bottom: 0,
+          Positioned(
+            child: Container(
+              height: 72,
+              decoration: BoxDecoration(
+                color: Color(0xffF7F8F7),
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(17),
+                  topLeft: Radius.circular(17),
+                ),
+              ),
+            ),
+            left: 0,
+            right: 0,
+            bottom: 0,
           ),
         ],
       ),
