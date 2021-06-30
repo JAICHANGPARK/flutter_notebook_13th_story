dbde944a658e932f6210ab969ed212f259cbd17b
Dreamwalker <aristojeff@gmail.com>
2021-06-30 23:06:22 +0900

    Update health_home_page.dart

---
+++ lib/ep833_health_app/ui/health_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HealthHomePage extends StatefulWidget {
+  const HealthHomePage({Key? key}) : super(key: key);
+
+  @override
+  _HealthHomePageState createState() => _HealthHomePageState();
+}
+
+class _HealthHomePageState extends State<HealthHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
