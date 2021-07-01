4efa7b464fe9e9e6780f0a9e9612c737a8c6d55d
Dreamwalker <aristojeff@gmail.com>
2021-07-01 21:14:43 +0900

    Update health_app_string.dart

--- lib/ep833_health_app/health_app_string.dart
+++ lib/ep833_health_app/health_app_string.dart
@@ -5,5 +5,6 @@ class HealthAppString{
   static const CHART_IMG = "assets/img/Chart.svg";
   static const DISCOVERY_IMG = "assets/img/Discovery.svg";
   static const PROFILE_IMG = "assets/img/Profile.svg";
+  static const NOTIFICATION_IMG = "assets/img/notification.svg";
 
 }
