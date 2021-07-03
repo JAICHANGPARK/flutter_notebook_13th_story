f069b9fcc04fba2c5c70dec834bd0b2bc05e4c2e
Dreamwalker <aristojeff@gmail.com>
2021-07-03 22:59:32 +0900

    Create grocery_home_page.dart

---
+++ lib/ep839_grocery_shop_app/grocery_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroceryHomePage extends StatefulWidget {
+  GroceryHomePage({Key key}) : super(key: key);
+
+  @override
+  _GroceryHomePageState createState() => _GroceryHomePageState();
+}
+
+class _GroceryHomePageState extends State<GroceryHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
