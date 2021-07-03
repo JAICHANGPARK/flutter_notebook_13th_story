9f32468736a602e6a3dde341579c246e1c66bc7d
Dreamwalker <aristojeff@gmail.com>
2021-07-03 23:13:48 +0900

    Update grocery_home_page.dart

--- lib/ep839_grocery_shop_app/grocery_home_page.dart
+++ lib/ep839_grocery_shop_app/grocery_home_page.dart
@@ -9,29 +9,55 @@ class GroceryHomePage extends StatefulWidget {
 
 class _GroceryHomePageState extends State<GroceryHomePage> {
   // 82 189 108
-  int _bottomTabIdx = 0;
+  ValueNotifier<int> _bottomTabIdx = ValueNotifier<int>(0);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xff08150D),
+      body: ValueListenableBuilder(
+        valueListenable: _bottomTabIdx,
+        builder: (BuildContext context, int value, Widget? child) {
+          switch (value) {
+            case 0:
+              return SafeArea(
+                child: Container(
+                  child: Center(
+                    child: Text("Page 1"),
+                  ),
+                ),
+              );
+            default:
+              return SafeArea(
+                child: Container(
+                  child: Center(
+                    child: Text("Page 2"),
+                  ),
+                ),
+              );
+          }
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
+        onTap: (idx) {
+          _bottomTabIdx.value = idx;
+        },
         backgroundColor: const Color(0xff08150D),
         type: BottomNavigationBarType.fixed,
-        selectedItemColor:const  Color(0xff52C66C),
+        selectedItemColor: const Color(0xff52C66C),
         unselectedItemColor: Colors.white,
-        selectedLabelStyle: const TextStyle(
-          fontSize: 12
-        ),
-        unselectedLabelStyle:  const TextStyle(
-            fontSize: 12
-        ),
+        selectedLabelStyle: const TextStyle(fontSize: 12),
+        unselectedLabelStyle: const TextStyle(fontSize: 12),
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.flutter_dash_outlined), label: "Categories"),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Favorite"),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: "Cart"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.flutter_dash_outlined), label: "Categories"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border), label: "Favorite"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_outlined), label: "Cart"),
         ],
-
       ),
     );
   }
