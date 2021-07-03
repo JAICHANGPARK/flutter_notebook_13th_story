754d5b0763ea0f9c928b03a8fe2a38ec981bd034
Dreamwalker <aristojeff@gmail.com>
2021-07-03 23:16:39 +0900

    Update grocery_home_page.dart

--- lib/ep839_grocery_shop_app/grocery_home_page.dart
+++ lib/ep839_grocery_shop_app/grocery_home_page.dart
@@ -71,28 +71,35 @@ class _GroceryHomePageState extends State<GroceryHomePage> {
           }
         },
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (idx) {
-          _bottomTabIdx.value = idx;
+      bottomNavigationBar:
+      ValueListenableBuilder(
+        valueListenable: _bottomTabIdx,
+        builder: (context, value, child){
+          return  BottomNavigationBar(
+            onTap: (idx) {
+             value = idx;
+            },
+            currentIndex:  _bottomTabIdx.value,
+            backgroundColor: const Color(0xff08150D),
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: const Color(0xff52C66C),
+            unselectedItemColor: Colors.white,
+            selectedLabelStyle: const TextStyle(fontSize: 12),
+            unselectedLabelStyle: const TextStyle(fontSize: 12),
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.flutter_dash_outlined), label: "Categories"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.favorite_border), label: "Favorite"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.shopping_cart_outlined), label: "Cart"),
+            ],
+          );
         },
-        currentIndex:  _bottomTabIdx.value,
-        backgroundColor: const Color(0xff08150D),
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: const Color(0xff52C66C),
-        unselectedItemColor: Colors.white,
-        selectedLabelStyle: const TextStyle(fontSize: 12),
-        unselectedLabelStyle: const TextStyle(fontSize: 12),
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home_outlined), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.flutter_dash_outlined), label: "Categories"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_border), label: "Favorite"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart_outlined), label: "Cart"),
-        ],
-      ),
+      )
+
     );
   }
 }
