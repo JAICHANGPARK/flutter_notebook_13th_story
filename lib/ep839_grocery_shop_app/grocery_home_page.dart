e82cfa6f675bd934acf36cbaf2344728d4bbab92
Dreamwalker <aristojeff@gmail.com>
2021-07-03 23:07:51 +0900

    Update grocery_home_page.dart

--- lib/ep839_grocery_shop_app/grocery_home_page.dart
+++ lib/ep839_grocery_shop_app/grocery_home_page.dart
@@ -18,11 +18,14 @@ class _GroceryHomePageState extends State<GroceryHomePage> {
         type: BottomNavigationBarType.fixed,
         selectedItemColor: Color(0xff52C66C),
         unselectedItemColor: Colors.white,
+        selectedLabelStyle: TextStyle(
+          fontSize: 12
+        ),
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.flutter_dash_outlined), label: "Categories"),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Categories"),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: "Categories"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite_border), label: "Favorite"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_outlined), label: "Cart"),
         ],
 
       ),
