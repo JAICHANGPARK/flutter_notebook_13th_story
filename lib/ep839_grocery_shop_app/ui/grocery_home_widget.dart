0e9be8ebcdfa74fe8a68c088d8830e3ab92a037b
Dreamwalker <aristojeff@gmail.com>
2021-07-04 20:32:51 +0900

    Update grocery_home_widget.dart

--- lib/ep839_grocery_shop_app/ui/grocery_home_widget.dart
+++ lib/ep839_grocery_shop_app/ui/grocery_home_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class GroceryHomeWidget extends StatefulWidget {
@@ -9,7 +8,8 @@ class GroceryHomeWidget extends StatefulWidget {
 }
 
 class _GroceryHomeWidgetState extends State<GroceryHomeWidget> {
-   Color _accentColor =  Color(0xff52C66C);
+  Color _accentColor = Color(0xff52C66C);
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -20,11 +20,14 @@ class _GroceryHomeWidgetState extends State<GroceryHomeWidget> {
               CircleAvatar(
                 backgroundColor: _accentColor,
                 child: Icon(Icons.menu),
-                foregroundColor: Colors.white
-                ,
+                foregroundColor: Colors.white,
               ),
               Container(),
-              CircleAvatar(  backgroundColor: _accentColor,),
+              CircleAvatar(
+                backgroundColor: _accentColor,
+                child: Icon(Icons.notifications_none_outlined),
+                foregroundColor: Colors.white,
+              ),
             ],
           )
         ],
