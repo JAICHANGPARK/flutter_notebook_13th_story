62b6f4bc0b721f2394c252635ac81ec1bf8297b7
Dreamwalker <aristojeff@gmail.com>
2021-07-04 20:28:31 +0900

    Update grocery_home_widget.dart

--- lib/ep839_grocery_shop_app/ui/grocery_home_widget.dart
+++ lib/ep839_grocery_shop_app/ui/grocery_home_widget.dart
@@ -1,2 +1,22 @@
 
 import 'package:flutter/material.dart';
+
+class GroceryHomeWidget extends StatefulWidget {
+  const GroceryHomeWidget({Key? key}) : super(key: key);
+
+  @override
+  _GroceryHomeWidgetState createState() => _GroceryHomeWidgetState();
+}
+
+class _GroceryHomeWidgetState extends State<GroceryHomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
