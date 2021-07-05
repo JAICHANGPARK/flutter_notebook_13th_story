9de9c6e3e3269fdaefff1dd22089621790fadee0
Dreamwalker <aristojeff@gmail.com>
2021-07-05 20:25:38 +0900

    Update grocery_home_widget.dart

--- lib/ep839_grocery_shop_app/ui/grocery_home_widget.dart
+++ lib/ep839_grocery_shop_app/ui/grocery_home_widget.dart
@@ -133,7 +133,7 @@ class _GroceryHomeWidgetState extends State<GroceryHomeWidget> {
                       color: Colors.white
                     ),),
                     CircleAvatar(
-                      backgroundColor: Color,
+                      backgroundColor: Color(0xff08150D),
                     )
                   ],
                 ),
