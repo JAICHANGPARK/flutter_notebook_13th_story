dc662cdb6feabd1ef71d6a3a03ad23ced563b665
Dreamwalker <aristojeff@gmail.com>
2021-07-06 20:44:31 +0900

    Create grocery_category_widget.dart

---
+++ lib/ep839_grocery_shop_app/ui/grocery_category_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroceryCategoryWidget extends StatefulWidget {
+  GroceryCategoryWidget({Key? key}) : super(key: key);
+
+  @override
+  _GroceryCategoryWidgetState createState() => _GroceryCategoryWidgetState();
+}
+
+class _GroceryCategoryWidgetState extends State<GroceryCategoryWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
