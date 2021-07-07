caa1ee8417925d1ad4d924b2b24e288a8aef8fe0
Dreamwalker <aristojeff@gmail.com>
2021-07-07 20:45:46 +0900

    Create meditation_class_main_page.dart

---
+++ lib/ep843_meditation_class_app/ui/meditation_class_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MeditationClassHomePage extends StatefulWidget {
+  const MeditationClassHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MeditationClassHomePageState createState() => _MeditationClassHomePageState();
+}
+
+class _MeditationClassHomePageState extends State<MeditationClassHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
