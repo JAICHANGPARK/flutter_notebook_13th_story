312ff0d1c8eec78ffc5fa20cbc539cd0214b38d4
Dreamwalker <aristojeff@gmail.com>
2021-07-07 20:46:10 +0900

    Update meditation_class_app.dart

--- lib/ep843_meditation_class_app/meditation_class_app.dart
+++ lib/ep843_meditation_class_app/meditation_class_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'ui/meditation_class_main_page.dart';
+
 class MeditationClassApp extends StatelessWidget {
   const MeditationClassApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: MeditationClassHomePage(),
     );
   }
 }
