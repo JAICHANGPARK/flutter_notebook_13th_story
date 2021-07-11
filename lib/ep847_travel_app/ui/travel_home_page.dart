30a1742146c69472c15b544fdc80aa11b36bfb71
Dreamwalker <aristojeff@gmail.com>
2021-07-11 20:37:57 +0900

    Update travel_home_page.dart

--- lib/ep847_travel_app/ui/travel_home_page.dart
+++ lib/ep847_travel_app/ui/travel_home_page.dart
@@ -11,38 +11,46 @@ class _TravelHomePageState extends State<TravelHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Positioned(
-              child: Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage("https://cdn.pixabay.com/photo/2020/06/08/20/58/nyc-5276112_960_720.jpg"),
-                        fit: BoxFit.cover)),
-              ),
-              left: 0,
-              right: 0,
-              top: 0,
-              bottom: 0,
+      body: Stack(
+        children: [
+          Positioned(
+            child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage("https://cdn.pixabay.com/photo/2020/06/08/20/58/nyc-5276112_960_720.jpg"),
+                      fit: BoxFit.cover)),
             ),
-            Positioned(
-              child: Container(
-                decoration: BoxDecoration(color: Colors.black.withOpacity(0.4), borderRadius: BorderRadius.circular(8)),
-                padding: EdgeInsets.all(8),
-                child: Center(
-                  child: Icon(
-                    Icons.keyboard_arrow_left,
-                    size: 32,
-                    color: Colors.white,
-                  ),
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+          ),
+          Positioned(
+            child: Container(
+              decoration: BoxDecoration(color: Colors.black.withOpacity(0.4), borderRadius: BorderRadius.circular(8)),
+              padding: EdgeInsets.all(8),
+              child: Center(
+                child: Icon(
+                  Icons.keyboard_arrow_left,
+                  size: 32,
+                  color: Colors.white,
                 ),
               ),
+            ),
+            left: 16,
+            top: 72,
+          ),
+          Positioned(
+
               left: 16,
-              top: 16,
-            )
-          ],
-        ),
+              right: 0,
+              bottom: 0,
+              child: Column(
+            children: [
+
+            ],
+          ))
+        ],
       ),
     );
   }
