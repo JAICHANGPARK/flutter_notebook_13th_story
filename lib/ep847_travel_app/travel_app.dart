fa6390c23a0bbe6d34807c30ff51bba68483dcc7
Dreamwalker <aristojeff@gmail.com>
2021-07-11 20:24:21 +0900

    Update travel_app.dart

--- lib/ep847_travel_app/travel_app.dart
+++ lib/ep847_travel_app/travel_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'ui/travel_home_page.dart';
+
 
 class TravelApp extends StatelessWidget {
   const TravelApp({Key? key}) : super(key: key);
