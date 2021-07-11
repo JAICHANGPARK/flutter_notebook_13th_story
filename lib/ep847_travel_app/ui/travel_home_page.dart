0d79f41c1d1edb0934a3b860e7e0a333bc512ace
Dreamwalker <aristojeff@gmail.com>
2021-07-11 20:35:52 +0900

    Update travel_home_page.dart

--- lib/ep847_travel_app/ui/travel_home_page.dart
+++ lib/ep847_travel_app/ui/travel_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TravelHomePage extends StatefulWidget {
   const TravelHomePage({Key? key}) : super(key: key);
 
@@ -12,34 +11,37 @@ class _TravelHomePageState extends State<TravelHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-
-          Positioned(child: Container(
-
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage("https://cdn.pixabay.com/photo/2020/06/08/20/58/nyc-5276112_960_720.jpg"),
-                fit: BoxFit.cover
-              )
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage("https://cdn.pixabay.com/photo/2020/06/08/20/58/nyc-5276112_960_720.jpg"),
+                        fit: BoxFit.cover)),
+              ),
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
             ),
-          ), left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,),
-          Positioned(child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white
-            ),
-            child: Center(
-              child: Icon(Icons.keyboard_arrow_left,
-              size: 32,),
-            ),
-          ),
-          left: 16,
-          top: 16,)
-
-        ],
+            Positioned(
+              child: Container(
+                decoration: BoxDecoration(color: Colors.black.withOpacity(0.4), borderRadius: BorderRadius.circular(8)),
+                padding: EdgeInsets.all(8),
+                child: Center(
+                  child: Icon(
+                    Icons.keyboard_arrow_left,
+                    size: 32,
+                  ),
+                ),
+              ),
+              left: 16,
+              top: 16,
+            )
+          ],
+        ),
       ),
     );
   }
