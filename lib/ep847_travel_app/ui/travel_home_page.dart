43a4652e19c9fcb9609b137aad2762bf8b363afa
Dreamwalker <aristojeff@gmail.com>
2021-07-11 20:26:40 +0900

    Update travel_home_page.dart

--- lib/ep847_travel_app/ui/travel_home_page.dart
+++ lib/ep847_travel_app/ui/travel_home_page.dart
@@ -15,6 +15,19 @@ class _TravelHomePageState extends State<TravelHomePage> {
       body: Stack(
         children: [
 
+          Positioned(child: Container(
+
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage("https://cdn.pixabay.com/photo/2020/06/08/20/58/nyc-5276112_960_720.jpg"),
+                fit: BoxFit.cover
+              )
+            ),
+          ), left: 0,
+          right: 0,
+          top: 0,
+          bottom: 0,)
+
         ],
       ),
     );
