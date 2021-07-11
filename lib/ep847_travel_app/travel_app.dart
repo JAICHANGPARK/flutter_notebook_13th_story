68a35ef0f0a1634884a41fe876df3225d8aa8910
Dreamwalker <aristojeff@gmail.com>
2021-07-11 20:18:54 +0900

    Update travel_app.dart

--- lib/ep847_travel_app/travel_app.dart
+++ lib/ep847_travel_app/travel_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 
 class TravelApp extends StatelessWidget {
@@ -6,6 +7,8 @@ class TravelApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+
+    );
   }
 }
