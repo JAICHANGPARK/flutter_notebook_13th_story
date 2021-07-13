f267d6481125e5ffd694e0760cd81966c9fae941
Dreamwalker <aristojeff@gmail.com>
2021-07-13 20:37:26 +0900

    Update crypto_wallet_app.dart

--- lib/ep849_crypto_wallet_app/crypto_wallet_app.dart
+++ lib/ep849_crypto_wallet_app/crypto_wallet_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'ui/crypto_wallet_home_page.dart';
+
 class CryptoWalletApp extends StatelessWidget {
   const CryptoWalletApp({Key? key}) : super(key: key);
 
