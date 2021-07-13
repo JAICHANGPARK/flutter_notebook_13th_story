4a775cdb9057738d72f7df617d49c8b8fb0a96c7
Dreamwalker <aristojeff@gmail.com>
2021-07-13 20:36:59 +0900

    Create crypto_wallet_home_page.dart

---
+++ lib/ep849_crypto_wallet_app/ui/crypto_wallet_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CryptoWalletHomePage extends StatefulWidget {
+  CryptoWalletHomePage({Key key}) : super(key: key);
+
+  @override
+  _CryptoWalletHomePageState createState() => _CryptoWalletHomePageState();
+}
+
+class _CryptoWalletHomePageState extends State<CryptoWalletHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
